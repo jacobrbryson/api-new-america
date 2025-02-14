815ef1de55011390bc7a1c9ae2cd851cecffda2e
Ross Bryson <jacobrbryson@gmail.com>
2025-02-13 19:08:45 -0600

    Updated login code

--- src/controllers/public/login.ts
+++ src/controllers/public/login.ts
@@ -1,47 +1,69 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 import { encodeToken } from "../../helpers/auth";
 import UserService from "../../services/user";
 import SteamService from "../../services/steam";
 
-export const login = async (req:Request, res:Response) => {
-  const steamId = req.body.steamId;
-  const displayName = req.body.displayName;
-  const sessionTicket = req.body.sessionTicket;
+export const login = async (req: Request, res: Response) => {
+	const steamId = req.body.steamId;
+	const displayName = req.body.displayName;
+	const sessionTicket = req.body.sessionTicket;
 
-  const verified = await verifySteamId(steamId, displayName, sessionTicket).catch((error) => {
-    console.error(error);
-  });
+	const verified = await verifySteamId(
+		steamId,
+		displayName,
+		sessionTicket
+	).catch((error) => {
+		console.error(error);
+	});
 
-  if(!verified) return res.status(403).json({message: "Invalid request, expected request body: {steamId:number, displayName:string}"});
-  
-  
-  const user = await UserService.login({steamId:steamId, displayName:displayName}).catch((error:any) => {
-    console.error(error);
-  });
+	if (!verified)
+		return res.status(403).json({
+			message:
+				"Invalid request, expected request body: {steamId:number, displayName:string}",
+		});
 
-  if(!user) return res.status(500).json({message: "Unable to create user"});
+	const user = await UserService.login({
+		steamId: steamId,
+		displayName: displayName,
+	}).catch((error: any) => {
+		console.error(error);
+	});
 
-  const token = encodeToken(user);
+	if (!user)
+		return res.status(500).json({ message: "Unable to create user" });
 
-  const response = {
-    accessToken: token,
-    user: user
-  }
+	const token = encodeToken(user);
 
-  return res.json(response);
-}
+	const response = {
+		accessToken: token,
+		user: user,
+	};
 
-async function verifySteamId(steamId:number, displayName:string, sessionTicket:string):Promise<boolean>{
-  if(isNaN(steamId) || !displayName?.length) throw({message: "Invalid login request body"});
+	return res.json(response);
+};
 
-  const data = await SteamService.authenticateUserTicket(sessionTicket).catch((error) => {
-    console.error(error);
-  });
+async function verifySteamId(
+	steamId: number,
+	displayName: string,
+	sessionTicket: string
+): Promise<boolean> {
+	if (isNaN(steamId) || !displayName?.length)
+		throw { message: "Invalid login request body" };
 
-  if(data?.response?.params?.result != 'OK') throw({message: "Steam auth API failure"});
-  if(data.response.params.steamid !== steamId) throw({message: "SteamId mis-match"});
-  if(data.response.params.vacbanned) throw({message: "User banned by valve"});
-  if(data.response.params.publisherbanned) throw({message: "User banned by publisher"});
+	const data = await SteamService.authenticateUserTicket(
+		sessionTicket
+	).catch((error) => {
+		console.error(error);
+	});
 
-  return true;
+	if (data?.response?.params?.result != "OK")
+		throw { message: "Steam auth API failure" };
+	if (data.response.params.steamid !== steamId)
+		throw { message: "SteamId mis-match" };
+	if (data.response.params.vacbanned)
+		throw { message: "User banned by valve" };
+	if (data.response.params.publisherbanned)
+		throw { message: "User banned by publisher" };
+
+	return true;
 }
--- src/services/steam.ts
+++ src/services/steam.ts
@@ -1,32 +1,31 @@
-import https from 'https';
-import config from '../config';
+import https from "https";
+import config from "../config";
 
 class Steam {
-	async authenticateUserTicket(sessionTicket:string):Promise<any>{
+	async authenticateUserTicket(sessionTicket: string): Promise<any> {
 		return new Promise((resolve, reject) => {
 			const options = {
-				host: config.steam.host ,
+				host: config.steam.host,
 				port: 443,
 				path: `/ISteamUserAuth/AuthenticateUserTicket/v1?key=${config.steam.key}&appid=${config.steam.appId}&ticket=${sessionTicket}&identity=${config.steam.identity}`,
-				method: 'GET'
+				method: "GET",
 			};
 
 			const request = https.get(options, (res) => {
-				res.on('data', function (result) {
-					if(res.statusCode == 200){
+				res.on("data", function (result) {
+					if (res.statusCode == 200) {
 						resolve(JSON.parse(result));
-					}else{
-						console.error(result);
+					} else {
+						reject(result);
 					}
 				});
 			});
 
-			request.on('error', function(e) {
-				console.error('Steam auth failed: ' + e.message);
+			request.on("error", function (e) {
+				reject("Steam auth failed: " + e.message);
 			});
-
 		});
 	}
 }
 
-export default new Steam()
+export default new Steam();
